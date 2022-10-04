d5429913690b2038760e57373c1c81943921cbda
Raúl Reymundi <raul@kickstarter.com>
2022-10-04 14:14:51 -0400

    commit tree 829e80eec2c630e0a271b9f2c38c92fd60c54023

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Maru', 3, 'F', 'dark brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Keke', 4, 'F', 'dark brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Gabi', 5, 'F', 'dark brown', 'serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Lisa', 3, 'F', 'dark brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Bob', 1, 'M', 'dark brown', 'serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Smokey', 6, 'M', 'dark brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Jordi', 7, 'M', 'dark brown', 'lazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'M', 'dark brown', 'friendly', 1);
